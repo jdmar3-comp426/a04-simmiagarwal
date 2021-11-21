2691358ac5e6616f277f67b4edc7ccf50b3d8507
Simmi <simmi@live.unc.edu>
2021-11-20 23:48:27 -0500

    print statements

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get("/app/users", (req, res) => {
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/users/:id/", (req, res) => {
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get([req.params.id]);
+	console.log(req.params.id);
 	res.status(200).json(stmt);
 })
 
